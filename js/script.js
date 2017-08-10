6ab71e4f38583acf712348e133aff6226cc6e803
Evoly <svetoch.sv@gmail.com>
2017-08-10 16:55:24 +0700

    iframe

--- js/script.js
+++ js/script.js
@@ -1,34 +1,49 @@
 
 // top-slider
 
-  $(function () {
-    $('#top-slider').carousel({
-        interval:4000,
-        pause: "hover",
-    }).on('slide.bs.carousel', function () {
-        document.getElementById('video').pause();
+$(function () {
+  $('#top-slider').carousel({
+      interval:4000,
+      pause: "hover",
   });
 });
 
+//init youtube API
 
-  $(document).ready(function(){
-      $('.another-slider').slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow: $('.js-central-slider-prev'),
-        nextArrow: $('.js-central-slider-next'),
-        responsive: [
-        {
-        breakpoint: 992,
-        settings: {
-          dots: false,
-        }
-      }
-      ]
-      });
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// Create YouTube player(s)
+var player1;
+
+function onYouTubeIframeAPIReady() {
+    player1 = new YT.Player('player1');
+}
+
+//
+$(document).ready(function () {
+    $(".js-bootstrap-btn").click(function () {
+        player1.stopVideo();
     });
+
+    $(this).mouseover(function(){
+      if (!$(this).hasClass("carousel")){
+        setTimeout(stopVideo, 4000);
+      }
+    });
+    function stopVideo() {
+        player1.stopVideo();
+      }
+
+});
+
+
+$('#top-slider').carousel().on('slide.bs.carousel', function (){
+  document.getElementById('video').pause();
+})
+
 
 // central-slider
 
